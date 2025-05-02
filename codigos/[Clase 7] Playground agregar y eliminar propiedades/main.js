b6a606923003ebb81da34274658802983a89e247
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-05-02 10:04:27 -0400

    [Clase 7]Playground agregar y eliminar propiedades

---
+++ codigos/[Clase 7] Playground agregar y eliminar propiedades/main.js
@@ -0,0 +1,51 @@
+const Input=
+[
+    {
+        name: "Product 1",
+        price: 1000,
+        stock: 10
+    },
+    {
+        name: "Product 2",
+        price: 2000,
+        stock: 20
+    }
+]
+
+// const Output=
+// [
+//     {
+//         name: "Product 1",
+//         price: 1000,
+//         stock: 10,
+//         taxes: 190
+//     },
+//     {
+//         name: "Product 2",
+//         price: 2000,
+//         stock: 20,
+//         taxes: 380
+//     }
+// ]
+
+const AgregarTaxes= (Input) => {
+    return Input.map((item) => ({
+        ...item,
+        'taxes': Math.trunc(item.price*0.19)
+    }))
+}
+
+const app1 = document.getElementById('app1')
+const output = AgregarTaxes(Input);
+
+app1.innerHTML = `
+<ul>
+    <li>Original: ${Input.map(item => 
+        `${item.name} (Precio: ${item.price * 2})`
+    ).join(', ')}</li>
+    
+    <li>Resultado: ${output.map(item => 
+        `${item.name} - Precio: ${item.price}, Stock: ${item.stock}, Impuestos: ${item.taxes}`
+    ).join(', ')}</li>
+</ul>
+`;
